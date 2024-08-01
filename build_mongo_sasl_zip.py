1f9e7b4317a0520fe3f405d48d2da21e07b6cb1b
Daniel Moody <dmoody256@gmail.com>
2024-08-01 22:00:54 +0000

    add script

---
+++ build_mongo_sasl_zip.py
@@ -0,0 +1,21 @@
+import os
+import shutil
+import glob
+
+shutil.rmtree("dist")
+
+os.makedirs("dist/bin")
+os.makedirs("dist/include/sasl")
+os.makedirs("dist/lib")
+
+shutil.copyfile("win32/x64/Release/sasl2.dll", "dist/bin/sasl2.dll")
+shutil.copyfile("win32/x64/Release/sasl2.pdb", "dist/bin/sasl2.pdb")
+shutil.copyfile("win32/x64/Release/sasl2.lib", "dist/lib/sasl2.lib")
+
+headers = glob.glob("include/*.h")
+headers.append("win32/include/md5global.h")
+for hdr in headers:
+    filename = os.path.basename(hdr)
+    shutil.copyfile(hdr, os.path.join("dist/include/sasl", filename))
+
+shutil.make_archive("sasl.zip", 'zip', "dist")
